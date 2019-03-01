ece07fd828e88a9e0ac0738b03e1dc7f53a8130e
Mergen Battur <mbattur@gmail.com>
2019-03-01 09:02:02 -0600

    continue

--- spec/factories/credit_card_debt.rb
+++ spec/factories/credit_card_debt.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :credit_card_debt do
     association :user
     card_name { 'capital one' }
-    balance { '333' }
+    balance { '11' }
   end
 end
--- spec/factories/stack_account.rb
+++ spec/factories/stack_account.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :stack_account do
     association :user
-    balance { '1222' }
+    balance { '1200' }
   end
 end
